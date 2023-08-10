05a15c428830c7bd3afc6005159a098cf92c95d9
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-08-10 01:30:50 +0000

    kudsgfkjdrsiuv

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/iron-man")
+def ironman():
+    return render_template("iron-man.html")
