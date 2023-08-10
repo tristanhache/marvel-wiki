18cf9ac098ff74ff8109e512815b53e53b23f86d
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-08-10 02:23:43 +0000

    dfdgffgtghggdhf

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
     
 @app.route("/iron-man")
 def ironman():
-    return render_template("iron-man.html")
+    return render_template("movies/marvel/iron-man/iron-man.html")
