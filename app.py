acee9f302f92fdd038576f4d3e99b76d4c6852fa
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-08-10 00:22:50 +0000

    fdhgfdhbff

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/iron-man")
+def ironman():
+    return render_template("iron-man.html")  
